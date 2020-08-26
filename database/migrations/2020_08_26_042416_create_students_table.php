ae5cc64bf39d82ab4320f458fc81eb2298b3a36f
Eliza Ahmed <elizarafee@gmail.com>
2020-08-26 10:47:24 +0600

    seeder created for students table

--- database/migrations/2020_08_26_042416_create_students_table.php
+++ database/migrations/2020_08_26_042416_create_students_table.php
@@ -23,7 +23,6 @@ class CreateStudentsTable extends Migration
             $table->string('created_by')->nullable();
             $table->string('updated_by')->nullable();
             $table->string('deleted_by')->nullable();
-            $table->timestamps();
         });
     }
 
---
+++ database/seeds/StudentsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Faker\Factory as faker;
+
+class StudentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('App\models\Students');
+        for ($i=0; $i<100; $i++) {
+            DB::table('students')-> insert([
+            'name' =>  $faker->name(),
+            'age' =>  $faker->numberBetween(18, 110),
+            'city' =>  $faker->city(),
+            'created_at' =>  \Carbon\Carbon::now(),
+            'updated_at' =>  \Carbon\Carbon::now(),
+            'deleted_at' =>  \Carbon\Carbon::now(),
+        ]);
+        }
+    }
+} 
