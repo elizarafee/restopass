0bcdf4c90061737ac84b5c07b1226a7865b9b9b2
Eliza Ahmed <elizarafee@gmail.com>
2020-09-01 15:32:08 +0600

    request created for login controller

---
+++ app/Http/Requests/StoreLoginRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreLoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|min:5|max:100',
+            'password' => 'required|min:5|max:15',
+        ];
+    }
+     /**
+     * Failed validation response
+     * 
+     * @return json 
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $data = array(
+            'status' => false,
+            'message' => 'Invalid Request',
+            'errors' => $validator->errors()
+        );
+
+        throw new HttpResponseException(response()->json($data, 422));
+    }
+}
